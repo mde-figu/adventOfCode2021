664fbbdc70a70ace3b07610f1ab6bcab551ee885
mde-figu <markusfwirz@gmail.com>
2021-12-02 15:55:06 -0300

    Second day, second star, thats no good

--- day1/main.c
+++ day1/main.c
@@ -11,17 +11,34 @@ unsigned int concatenate(unsigned int x, unsigned int y)
 	return x * pow + y;
 }
 
+/* int partTwo(int actual, int count)
+{
+	if(count %3)
+}
+ */
 int	main(void)
 {
 	FILE *fp;
    int c;
    int increase;
    int decrease;
+	int blockIncrease;
+   int blockDecrease;
    unsigned int	i;
+   unsigned int forelast;
    unsigned int last;
    unsigned int actual;
+   int *arrTmp;
+   int block1;
+   int block2;
+   int	j;
 
-
+	j = 1;
+	block1 = 0;
+	block2 = 0;
+	blockIncrease = 0;
+	blockDecrease = 0;
+	forelast = 0;
 	last = 0;
 	actual = 0;
 	increase = 0;
@@ -39,21 +56,42 @@ int	main(void)
 		{
 			//printf("%i", i);
 			actual = concatenate(actual, i);
-			//printf(":%i", actual);
 		}
 		else
 		{
+			//printf("%i\t", forelast);
+			//printf("%i\t", last);
+			//printf("%i\t", actual);
 			if (actual >= last && actual != 0)
 				increase++;
 			else if (actual <= last && actual != 0)
 				decrease++;
+			if (j <= 3)
+				block1 = block1 + actual;
+			else if (j > 3)
+			{
+				block1 = forelast + last + actual;
+			}
+			forelast = last;
 			last = actual;
 			actual = 0;
-			//printf("\n");
+			if (j >= 3)
+			{
+				if (block1 > block2 && block2 != 0)
+					blockIncrease++;
+				else if (block1 < block2 && block2 != 0)
+					blockDecrease++;
+				//printf("\n");
+				printf("%i\t %i\t inc%i\n", block2, block1, blockIncrease);
+				block2 = block1;
+				block1 = 0;
+			}
+			j++;
 		}
 		i = 0;
    }
-   printf("Increments:%i Decrements:%i", increase - 1, decrease);
+   printf("Part 1 = Increments:%i Decrements:%i\n", increase - 1, decrease);
+   printf("Part 2 = Block Increments:%i Block Decrements:%i\n", blockIncrease, blockDecrease);
    fclose(fp);
    
    return(0);
